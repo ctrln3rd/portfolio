a71a7e2879ae37c9da16a2e4eaef25ee1f7b5015
austinemark001 <markaustine001@gamil.com>
2025-03-04 19:51:14 +0300

    changing home titl name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
 import Home from "@/components/home"
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: 'austine mark',
-  
-}
 
 export default function HomePage() {
   return <Home/>
