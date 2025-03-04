11cfc55340c185a6a572d4f0ebe6535089fcec03
austinemark001 <markaustine001@gamil.com>
2025-03-04 18:38:17 +0300

    adding seo tools

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import Contact from "@/components/contact";
 import { Metadata } from "next";
 
 export const  metadata: Metadata = {
-    title: 'Connect With ',
+    title: 'Connect With',
 }
 
 export default function ContactPage(){
